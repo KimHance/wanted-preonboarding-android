11762621fb17349e509f567da6ac7a8fefd09410
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-07 00:36:25 +0900

    feat Retrofit 모듈 생성

---
+++ app/src/main/java/com/wanted/wanted_news/base/di/RetrofitModule.kt
@@ -0,0 +1,40 @@
+package com.wanted.wanted_news.base.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    companion object {
+        private const val BASE_URL = "https://newsapi.org/v2/"
+    }
+}
