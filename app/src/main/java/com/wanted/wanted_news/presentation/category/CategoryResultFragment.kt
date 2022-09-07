42bef7a95229bd86bceabda8f03c83052a9b0879
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-08 05:37:39 +0900

    feature 로딩 추가
    HTTP 429 방지

--- app/src/main/java/com/wanted/wanted_news/presentation/category/CategoryResultFragment.kt
+++ app/src/main/java/com/wanted/wanted_news/presentation/category/CategoryResultFragment.kt
@@ -8,18 +8,23 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.paging.LoadState
 import com.wanted.wanted_news.R
 import com.wanted.wanted_news.base.BaseFragment
 import com.wanted.wanted_news.databinding.FragmentCategoryResultBinding
 import com.wanted.wanted_news.domain.News
+import com.wanted.wanted_news.presentation.LoadingDialog
 import com.wanted.wanted_news.presentation.MainViewModel
 import com.wanted.wanted_news.presentation.adapter.NewsAdapter
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class CategoryResultFragment :
     BaseFragment<FragmentCategoryResultBinding>(R.layout.fragment_category_result) {
 
+    private lateinit var loadingDialog: LoadingDialog
     private val resultViewModel: MainViewModel by activityViewModels()
     private val newsAdapter: NewsAdapter by lazy {
         NewsAdapter(
@@ -29,6 +34,7 @@ class CategoryResultFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadingDialog = LoadingDialog(requireContext())
         initView()
         fetchNews()
         collectFlow()
@@ -46,6 +52,15 @@ class CategoryResultFragment :
                 resultViewModel.newsList.collectLatest { newsList ->
                     newsAdapter.submitData(newsList)
                 }
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsAdapter.loadStateFlow
+                    .collectLatest {
+                        if (it.source.append is LoadState.Loading) loadingDialog.show()
+                        else loadingDialog.dismiss()
+                    }
             }
         }
     }
