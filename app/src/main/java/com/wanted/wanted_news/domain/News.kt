29ad96bf302f77f27ae526a68308411c37d36d02
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-07 20:41:16 +0900

    fix default value 추가

--- app/src/main/java/com/wanted/wanted_news/domain/News.kt
+++ app/src/main/java/com/wanted/wanted_news/domain/News.kt
@@ -1,10 +1,10 @@
 package com.wanted.wanted_news.domain
 
 data class News(
-    val title: String,
-    val author: String,
-    val content: String,
-    val imageUrl: String,
-    val time: String,
+    val title: String = "",
+    val author: String = "",
+    val content: String = "",
+    val imageUrl: String = "",
+    val time: String = "",
     val isSaved: Boolean = false
 )
