238190c061bac470952fae3c580965cee1e28a4f
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-08 02:36:07 +0900

    feature action으로 news 넘겨주기

--- app/src/main/java/com/wanted/wanted_news/domain/News.kt
+++ app/src/main/java/com/wanted/wanted_news/domain/News.kt
@@ -1,5 +1,9 @@
 package com.wanted.wanted_news.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class News(
     val title: String? = "",
     val author: String? = "",
@@ -7,4 +11,4 @@ data class News(
     val imageUrl: String? = "",
     val time: String? = "",
     val isSaved: Boolean = false
-)
+) : Parcelable
