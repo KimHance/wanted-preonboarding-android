5fcfe02ca7052a669b079ff6c836f6f9fe515fa3
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-07 22:06:50 +0900

    fix glide error 처리 및 xml 수정

--- app/src/main/java/com/wanted/wanted_news/presentation/adapter/BindingAdapter.kt
+++ app/src/main/java/com/wanted/wanted_news/presentation/adapter/BindingAdapter.kt
@@ -3,10 +3,12 @@ package com.wanted.wanted_news.presentation.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.wanted.wanted_news.R
 
 @BindingAdapter("bindImage")
 fun ImageView.bindImage(imageUrl: String?) {
     imageUrl?.let {
-        Glide.with(this).load(imageUrl).into(this)
+        Glide.with(this).load(imageUrl).error(R.drawable.ic_baseline_image_not_supported)
+            .centerCrop().into(this)
     }
 }
