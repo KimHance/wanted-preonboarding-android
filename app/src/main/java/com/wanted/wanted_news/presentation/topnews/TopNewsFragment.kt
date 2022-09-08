b5ab25cf8aa1adf6d26048593c3885b7f99a9dcb
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-09-08 14:09:10 +0900

    Revert "feature 로딩 추가"
    This reverts commit 42bef7a95229bd86bceabda8f03c83052a9b0879.

--- app/src/main/java/com/wanted/wanted_news/presentation/topnews/TopNewsFragment.kt
+++ app/src/main/java/com/wanted/wanted_news/presentation/topnews/TopNewsFragment.kt
@@ -7,12 +7,10 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.findNavController
-import androidx.paging.LoadState
 import com.wanted.wanted_news.R
 import com.wanted.wanted_news.base.BaseFragment
 import com.wanted.wanted_news.databinding.FragmentTopNewsBinding
 import com.wanted.wanted_news.domain.News
-import com.wanted.wanted_news.presentation.LoadingDialog
 import com.wanted.wanted_news.presentation.MainViewModel
 import com.wanted.wanted_news.presentation.adapter.NewsAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,8 +19,6 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TopNewsFragment : BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_top_news) {
-
-    private lateinit var loadingDialog: LoadingDialog
     private val mainViewModel: MainViewModel by activityViewModels()
     private val newsAdapter: NewsAdapter by lazy {
         NewsAdapter(
@@ -32,7 +28,6 @@ class TopNewsFragment : BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_t
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        loadingDialog = LoadingDialog(requireContext())
         mainViewModel.getNewsList(null)
         initView()
         collectFlow()
@@ -50,15 +45,6 @@ class TopNewsFragment : BaseFragment<FragmentTopNewsBinding>(R.layout.fragment_t
                 mainViewModel.newsList.collectLatest { newsList ->
                     newsAdapter.submitData(newsList)
                 }
-            }
-        }
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                newsAdapter.loadStateFlow
-                    .collectLatest {
-                        if (it.source.append is LoadState.Loading) loadingDialog.show()
-                        else loadingDialog.dismiss()
-                    }
             }
         }
     }
